3e188a897be22f0859fbfdff66f8ac17a3f1d8ff
andrewokogu <ejirookogu@gmail.com>
2018-03-09 16:13:08 +0000

    up

---
+++ lab6/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World"
+    echo "Hello," . "" . "world" . "!";
+    echo 5*7;
+    $myname = "Frodo Baggins";
+    $myage = 111;
+    echo "My name is" . $myname . " and I am " . $myage;
+    echo " i get printed!";
+    ?>
+    <?php
+    
+    ?>
+
+</p>
+</body>
+</html>
